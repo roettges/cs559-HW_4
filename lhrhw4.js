a955c717148d140bf40c198ef7ccf74409a30b34
roettges <laura.h.roettges@gmail.com>
2023-10-17 22:50:53 -0500

    got lines to draw

--- lhrhw4.js
+++ lhrhw4.js
@@ -4,8 +4,7 @@ function setup() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     var context = canvas.getContext('2d');
-    var slider1 = document.getElementById('slider1');
-    slider1.value = 0;
+    time = 0; 
 
     function moveToTx(loc,Tx)
     {var res=vec2.create(); vec2.transformMat3(res,loc,Tx); context.moveTo(res[0],res[1]);} // reusing professors initial move to and line to functions to get x,y based on the passed in stack aka Tx
@@ -158,13 +157,12 @@ function setup() {
 
     function generateBigP(controlary) {
             var bigP=[];
-            console.log(controlary.length);
             for (let i=0; i < controlary.length-3; i+=2) {
                 var big_p_next = [controlary[i],controlary[i+1],controlary[i+2],controlary[i+3]];
                 bigP.push(big_p_next);
             }
             return bigP;
-            console.log('BigP: at creation' +bigP)
+            //console.log('BigP: at creation' +bigP)
         } // return P matrix should ensure C1 continuity with Hermite
 
     var bees = []
@@ -196,33 +194,14 @@ function setup() {
         }
         var plast = [((Math.random()-.5)*2*canvas.width),((Math.random()-.5)*2*canvas.width)]
         controls.push(plast);
-        console.log(controls);
+        //console.log(controls);
         var pMatrices = generateBigP(controls);
 
         bees.push(new Bee(controls,pMatrices,Hermite,TangentHermite,moveToTx,lineToTx,ellipseToTx))
     }
 
     function Cubic(B,pset,t) {
-        console.log('pset: '+pset);
-        console.log('pset[0]: '+pset[0]);
-        console.log('pset[1]: '+pset[1]);
-        console.log('pset[2]: ' +pset[2]);
-        console.log('pset[3]: '+pset[3]);
         var b = B(t);
-        console.log('what is B:'+b)
-        console.log('b of 0: ' +b[0])
-        console.log('b of 1: ' +b[1])
-        console.log('b of 2: ' +b[2])
-        console.log('b of 3: ' +b[3])
-        console.log('pset o of o:' +pset[0][0])
-        console.log('pset o of o:' +pset[0][1])
-        console.log('pset o of o:' +pset[1][0])
-        console.log('pset o of o:' +pset[1][1])
-        console.log('pset o of o:' +pset[1][1])
-        console.log('pset o of o:' +pset[2][0])
-        console.log('pset o of o:' +pset[2][1])
-        console.log('pset o of o:' +pset[3][0])
-        console.log('pset o of o:' +pset[3][1])
         var result=vec2.create();
         vec2.scale(result,[pset[0][0],pset[0][1]],b[0]); //1st point
         vec2.scaleAndAdd(result,result,[pset[1][0],pset[1][1]],b[1]); //1st deriv
@@ -233,65 +212,57 @@ function setup() {
 
   
     var generatePiecewiseC = function(B,i,t) {
-            console.log('what is i: ' +i)
-            console.log('what is bgP now:' + bees[0].bigP[0][i-1])
-            //B in my example will be either the basis or the tangent basis (trying to make this reusable
-            var piecewiseC =(Cubic(B,bees[0].bigP[i-1][i-1],t)); //bigP[0][i] is a set of 4 sets of 2: 2 points/2 tangents
-            console.log('WHAT IS C LOOK AT ME:' + piecewiseC)
-            return piecewiseC;
-        }
+        //B in my example will be either the basis or the tangent basis (trying to make this reusable
+        var piecewiseC =(Cubic(B,bees[0].bigP[0][i-1],t)); //bigP[0][i] is a set of 4 sets of 2: 2 points/2 tangents
+        return piecewiseC;
+    }
 
     var compRestraintCPath = function(B,t) {
-            for (let j=1; j <= bees[0].bigP[0].length; j +=1) {
-                console.log('IS j INCREMENTING AT ALL?: ' +j + 'length?: ' +bees[0].bigP[0].length)
-                if (t<j) {
-                    var u = t-(j-1);
-                    var pieceC=generatePiecewiseC(B,j,u);
-                    //var pieceC = function(u){Cubic(B,bees[0].bigP[0][i-1],u)};
-                    console.log('GRRRRrRRRRRRRRRRRR: ' + pieceC)
-                        //function(B,t){Cubic(B,bees[0].bigP[0][i-1],t)})
-                    return pieceC;
-                    //return generatePiecewiseC(B,i,u);
-                } else {
-                    var u = t-(bees[0].bigP[0].length-1);
-                    var pieceC = function(u){Cubic(B,bees[0].bigP[0][j-1],u)};
-                    console.log('AHHHHHHH: ' + pieceC);
-                        //function(B,t){Cubic(B,bees[0].bigP[0][i-1],t)})
-                    return pieceC;
-                    //return generatePiecewiseC(B,i,u); //need to come back and edit this and just get the drawing to disapear ideally
-                } //end else   
-            } //end for
-        } 
+        var len = bees[0].bigP[0].length;
+        if (t>=len) {
+            var u = t-(len-1);
+            var pieceC = generatePiecewiseC(B,len,u);
+        } else {
+            var j = Math.floor(t); //my step is always 1 in this scenario, u will be between 0 &1
+            var u = t-j
+            var pieceC = generatePiecewiseC(B,j+1,u);
+        }
+        return pieceC;
+    } 
 
-    function drawTrajectory(t_begin,t_end,intervals,C,Tx,color) {
-            context.strokeStyle=color;
-            context.beginPath();
-            moveToTx(compRestraintCPath(Hermite,t_begin),Tx);
-            for(var i=1;i<=intervals;i++){
-                var t=((intervals-i)/intervals)*t_begin+(i/intervals)*t_end;
-                lineToTx(compRestraintCPath(Hermite,t),Tx);
-            }
-            context.stroke();
-        };
+    function drawTrajectory(t_begin,t_end,intervals,Tx,color) {
+        context.strokeStyle=color;
+        context.beginPath();
+        moveToTx(compRestraintCPath(Hermite,t_begin),Tx);
+        for(var i=1;i<=intervals;i++){
+            var t=((intervals-i)/intervals)*t_begin+(i/intervals)*t_end;
+            lineToTx(compRestraintCPath(Hermite,t),Tx);
+        }
+        context.stroke();
+    };
     console.log('My Bee: ' + bees[0])
     console.log(bees[0].bigP)
-    var p=bees[0].bigP;
-    //p.push(bees[0].bigP)
-    console.log('P:' + p);
     console.log(bees[0].bigP[0].length)
-    console.log('here: '+ p[0][0]);
+   // console.log('here: '+ p[0][0]);
   
 
     var Tbase_to_canvas = mat3.create();
     mat3.fromTranslation(Tbase_to_canvas,[0,canvas.height]); //move to bottom left
     mat3.scale(Tbase_to_canvas,Tbase_to_canvas,[1,-1]); // Flip the Y-axis
     drawAxes("blue",Tbase_to_canvas);
-    drawTrajectory(0.0,1.0,100,compRestraintCPath,Tbase_to_canvas,"red");
-    drawTrajectory(1.0,9.0,100,compRestraintCPath,Tbase_to_canvas,"red");
+    drawTrajectory(0.0,6.0,200,Tbase_to_canvas,"red");
+    //drawTrajectory(1.0,2.0,10,Tbase_to_canvas,"red"); console.log("I made it here?!?!?!?!")
+    //drawTrajectory(1.0,9.0,100,compRestraintCPath,Tbase_to_canvas,"red");
 
+    /*function update(time) {
+        draw(time);
+    }
 
-   /* function animate () {
-        canvas.width=canvas.width;
+    function moveBee() {
+        //canvas.width=canvas.width;
+        if (time > 7) {time = 0;}
+        time = time +0.01;
+        update(time);
     }*/
 
     canvas.addEventListener('click',(event) => {
